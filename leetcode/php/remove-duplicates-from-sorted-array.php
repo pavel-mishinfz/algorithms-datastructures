f77247afef6fcc68b7ef575711f1cd95213f3acb
pavel.mishinfz <pavel.mishinfz@gmail.com>
2023-06-27 19:37:21 +0300

    Time: 21 ms (89.5%), Space: 21.1 MB (71.57%)

---
+++ leetcode/php/remove-duplicates-from-sorted-array.php
@@ -0,0 +1,23 @@
+<?php
+class Solution {
+
+    /**
+     * @param Integer[] $nums
+     * @return Integer
+     */
+    function removeDuplicates(&$nums) {
+        $k = 0;
+        $cur_num_idx = 0;
+        while($cur_num_idx < count($nums)) {
+            $next_num_idx = $cur_num_idx;
+            $nums[$k++] = $nums[$cur_num_idx];
+            while($next_num_idx < count($nums) 
+            && $nums[$next_num_idx] === $nums[$cur_num_idx]) 
+            {
+                ++$next_num_idx;
+            }
+            $cur_num_idx = $next_num_idx;   
+        }
+        return $k;
+    }
+}
